0fe8832caa045735bbf81441736ab769e8a9f5b0
Carine Neris <carine_neris@hotmail.com>
2019-06-16 19:43:52 -0300

    Aula 10

--- tw_clientes/clientes/urls.py
+++ tw_clientes/clientes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
     path('clientes', listar_clientes, name='listar_cliente'),
     path('clientes/cadastrar', inserir_cliente, name='cadastrar_cliente'),
