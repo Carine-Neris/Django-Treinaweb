072b367f71187cc2f8ecae5e583507f313ecdbda
Carine Neris <carine_neris@hotmail.com>
2019-06-27 23:07:16 -0300

    Fim da aula 12, criado view,template e url para edicao do cliente cadastrado

--- tw_clientes/clientes/urls.py
+++ tw_clientes/clientes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('clientes', listar_clientes, name='listar_cliente'),
     path('clientes/cadastrar', inserir_cliente, name='cadastrar_cliente'),
     path('clientes/listar/<int:id>', listar_cliente_id, name='listar_cliente_id'),
+    path('clientes/editar/<int:id>', editar_cliente, name='editar_cliente'),
 ]
