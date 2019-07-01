4b01cc339dcac80178d746c2ebc06b34df14b933
Carine Neris <carine_neris@hotmail.com>
2019-06-30 21:01:44 -0300

    final da aula 14

--- tw_clientes/clientes/urls.py
+++ tw_clientes/clientes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('clientes/cadastrar', inserir_cliente, name='cadastrar_cliente'),
     path('clientes/listar/<int:id>', listar_cliente_id, name='listar_cliente_id'),
     path('clientes/editar/<int:id>', editar_cliente, name='editar_cliente'),
+    path('clientes/remover/<int:id>', remover_cliente, name='remover_cliente'),
 ]
